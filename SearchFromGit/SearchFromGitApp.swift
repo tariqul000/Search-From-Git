94d1e76c343099773d0a9175c6a2ffc744ae7281
tariqul00 <tariqul1993@gmail.com>
2023-03-07 17:05:10 +0600

    Initial Commit

---
+++ SearchFromGit/SearchFromGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchFromGitApp.swift
+//  SearchFromGit
+//
+//  Created by Tariqul Islam on 7/3/23.
+//
+
+import SwiftUI
+
+@main
+struct SearchFromGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
